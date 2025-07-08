327ad3bd171ac9d4d4882e8bb574bb0b4b49da1c
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-07-08 19:59:27 +0700

    Thoai commit

--- obj/Debug/net8.0/Insurance_agency.AssemblyInfo.cs
+++ obj/Debug/net8.0/Insurance_agency.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d020db5c4bb99db63a00cdfd9fc9f7f3362bbe3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62af3578c36f4216f6d5d91c44ac8e4480efb004")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
