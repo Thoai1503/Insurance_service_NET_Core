247db65afb13e67f0accabd1da9d06337e0ebcad
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-07-12 17:20:02 +0700

    update VnPay config

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Insurance_agency.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
