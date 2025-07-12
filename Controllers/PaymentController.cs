9038cc40be079cc11e2ce5e454db78e6405b1cad
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-07-12 17:20:52 +0700

    add payment controller

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Insurance_agency.Models.ModelView;
+using Insurance_agency.Services.VnPay;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Insurance_agency.Controllers
 {
     public class PaymentController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IVnPayService _vnPayService;
+        public PaymentController(IVnPayService vnPayService)
         {
-            return View();
+
+            _vnPayService = vnPayService;
         }
+
+        public IActionResult CreatePaymentUrlVnpay(PaymentInformationModel model)
+        {
+            var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
+
+            return Redirect(url);
+        }
+        [HttpGet]
+        public IActionResult PaymentCallbackVnpay()
+        {
+            var response = _vnPayService.PaymentExecute(Request.Query);
+
+            return Json(response);
+        }
+
+
     }
 }
