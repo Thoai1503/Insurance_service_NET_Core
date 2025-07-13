b3cab5d51fa4a196b9aec165c1100a7f2983aaa3
Hoàng Long <hoanglongngl98@gmail.com>
2025-07-13 18:39:01 +0700

    13/7

--- obj/Debug/net8.0/Insurance_agency.AssemblyInfo.cs
+++ obj/Debug/net8.0/Insurance_agency.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3872211585f0552ef113f4829c33b7e7c99cffb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffbf80357b5e6ab96391a6fce9aa8e1bbc7c062")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
