b88c4e16df0d30151099284b8b813d27fc2a1238
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-07-25 10:10:47 +0700

    change

--- Controllers/LoanController.cs
+++ Controllers/LoanController.cs
@@ -1,4 +1,5 @@
-﻿using Insurance_agency.Models.Repository;
+﻿using Insurance_agency.Models.ModelView;
+using Insurance_agency.Models.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Insurance_agency.Controllers
@@ -13,6 +14,36 @@ namespace Insurance_agency.Controllers
         {
             var contract = ContractRepository.Instance.FindById(contractId);
             return View(contract);
+        }
+        public IActionResult MakeRequest(int contractId, int amount)
+        {
+            var contract = ContractRepository.Instance.FindById(contractId);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            // Assuming you have a method to create a loan request
+            var loanRequest = new Loan
+            {
+                ContractId = contractId,
+                LoanAmount = amount,
+
+                RequestDate = DateTime.Now,
+                Status = 0 // Assuming 0 means pending
+
+            };
+            var result = LoanRepository.Instance.Create(loanRequest);
+            if (!result) 
+            {
+                // Handle the case where the loan request could not be created
+                return BadRequest("Could not create loan request.");
+            }else
+            {
+                return RedirectToAction("Index","UserProfile");
+            }
+            // Save the loan request to the database (not shown here)
+            // LoanRepository.Instance.Add(loanRequest);
+          
         }
     }
 }
---
+++ Models/Repository/LoanRepository.cs
@@ -0,0 +1,78 @@
+﻿using Insurance_agency.Models.Entities;
+using Insurance_agency.Models.ModelView;
+
+namespace Insurance_agency.Models.Repository
+{
+    public class LoanRepository : IRepository<Loan>
+    {
+        private static LoanRepository _instance;
+        private readonly InsuranceContext _context;
+        private LoanRepository()
+        {
+            _context = new InsuranceContext();
+        }
+        public static LoanRepository Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new LoanRepository();
+                }
+                return _instance;
+            }
+        }
+        public bool Create(Loan entity)
+        {
+        
+
+
+            try
+            {
+                var loan = new TblLoan
+                {
+                    ContractId = entity.ContractId,
+                    LoanAmount = entity.LoanAmount,
+                    RequestDate = entity.RequestDate,
+                    Status = entity.Status
+                };
+                _context.TblLoans.Add(loan);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex) as needed
+                return false;
+            }
+
+
+
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Loan FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<Loan> FindByKeywork(string keywork)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<Loan> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Loan entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
