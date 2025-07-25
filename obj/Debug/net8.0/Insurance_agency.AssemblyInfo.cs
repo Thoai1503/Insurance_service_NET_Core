a78394fe05e4bb6fcf8b8cf26110c8202c8d78df
minhphat1612 <minhphat1612@gmail.com>
2025-07-25 18:36:14 +0700

    change

--- obj/Debug/net8.0/Insurance_agency.AssemblyInfo.cs
+++ obj/Debug/net8.0/Insurance_agency.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd46604897ef2ab67235b7c1c7e33f0dbff96b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde4db832a0b7e98d4e7f1fe3921878f8c6076de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insurance_agency")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
